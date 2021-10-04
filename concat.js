564eed495f1a005b6d7e8891cc30acb4428b86f0
Mohamed Samir <mosamircs@gmail.com>
2021-10-04 20:47:56 +0200

    spreed rest capitalize

---
+++ concat.js
@@ -0,0 +1,4 @@
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+const produce = fruits.concat(vegetables);
+console.log(produce)
---
+++ rest-parameter.js
@@ -0,0 +1,3 @@
+const order = [20.17, 18.67, 1.50, "cheese", "eggs", "milk", "bread"];
+const [total, subtotal, tax, ...items] = order;
+console.log(total, subtotal, tax, items);
